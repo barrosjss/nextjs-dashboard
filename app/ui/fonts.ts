749561ce232cd6bc1005306752fd8bc3f6588b21
tract0rb <jesus.reinaldo.barros@gmail.com>
2024-03-24 21:44:21 -0500

    Update fonts.ts

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,3 +1,10 @@
-import { Montserrat } from "next/font/google";
+import { Montserrat, Inter, Lusitana } from 'next/font/google';
 
-export const montserrat = Montserrat({ subsets: ["latin"] });
+export const montserrat = Montserrat({ subsets: ['latin'] });
+
+export const inter = Inter({ subsets: ['latin'] });
+
+export const lusitana = Lusitana({
+  weight: ['400', '700'],
+  subsets: ['latin'],
+});
